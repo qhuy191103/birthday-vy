97db4358d93a1c81fb69748ae5ee180990de49aa
qhuy191103 <nguyenlamquanghuy.2003@gmail.com>
2025-07-11 02:52:15 +0700

    aaaaaa2

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
+// 👇 THÊM base ở đây — thay 'ten-repo-cua-ban' bằng tên repo GitHub của bạn
 export default defineConfig({
+  base: '/birthday-vy/',
   plugins: [tailwindcss(), react()],
 })
